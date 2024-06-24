9a099b25d3480d57201f88bbe194e59ead673cb6
Isabella <isabellaotoo25@gmail.com>
2024-06-24 20:28:39 +0000

    Update Navbar.js

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg header-gradient p-2 fixed-top">
   <div className="container-fluid">
-    <Link to="/" className="navbar-brand text-white">Fly Study Thrive</Link>
+    <Link to="/" className="navbar-brand text-white">FlyStudyThrive</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
