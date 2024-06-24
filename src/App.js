2818a804bba51932ca9099b3188499d4f9bd3352
Isabella <isabellaotoo25@gmail.com>
2024-06-24 20:22:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/inc/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
