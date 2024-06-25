329b543d583db21138cbb0c084bb1cab580c3524
Isabella <isabellaotoo25@gmail.com>
2024-06-25 09:23:10 +0000

    Update Banner.js

--- src/components/inc/Banner.js
+++ src/components/inc/Banner.js
@@ -9,6 +9,7 @@ const Banner = () => {
 
     return (
         <div className="alert alert-info alert-dismissible fade show" role="alert">
+            <h2 className='closable-banner-title'>We've launched our new book: FlyStudyThrive Experts Handbook to College Admission! </h2>
             <strong>Notice!</strong> This is an important notice for all users. Please read it carefully.
             <button type="button" className="btn-close" aria-label="Close" onClick={() => setShow(false)}></button>
         </div>
